13e261fb84b2000d790ed4549da86c7761599b8c
witskeeper <witskeeper@gmail.com>
2025-04-12 00:07:51 +0800

    v1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -104,7 +104,7 @@ const Header = () => {
                 {t('header.events')}
               </Link>
               <div className="px-4 py-2">
-                <Button className="w-full">
+                <Button className="w-full" onClick={toggleQrModal}>
                   {t('header.joinCommunity')}
                 </Button>
               </div>
